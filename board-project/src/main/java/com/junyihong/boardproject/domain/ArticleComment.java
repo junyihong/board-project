a480798b03135e571f48676f09c1d97f1b6b78cd
junyihong <junyihong@naver.com>
2023-07-28 16:56:36 +0900

    #1 - 필요없는 import문 삭제
    AuditingFields파일을 추출하면서 ArticleComment파일에 남아있던 불필요한 import문을 정리하였다.
    JpaRepositoryTest 파일에서도 불필요한 import문을 정리하였다.

--- board-project/src/main/java/com/junyihong/boardproject/domain/ArticleComment.java
+++ board-project/src/main/java/com/junyihong/boardproject/domain/ArticleComment.java
@@ -2,16 +2,9 @@ package com.junyihong.boardproject.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
--- board-project/src/test/java/com/junyihong/boardproject/repository/JpaRepositoryTest.java
+++ board-project/src/test/java/com/junyihong/boardproject/repository/JpaRepositoryTest.java
@@ -5,16 +5,15 @@ import com.junyihong.boardproject.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-//@ActiveProfiles("testdb")
+
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
