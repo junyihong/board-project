63e265ebc72a29e5836ef3abc4a7ab234c036f62
junyihong <junyihong@naver.com>
2023-07-30 14:14:38 +0900

    #18 -  게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ board-project/src/main/java/com/junyihong/boardproject/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.junyihong.boardproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
