063efdb9ca37656a18a8ade144ea2f5aa2c6decf
junyihong <junyihong@naver.com>
2023-07-30 14:17:10 +0900

    #18 - 리팩토링 : 불필요한 import문 제거

--- board-project/src/test/java/com/junyihong/boardproject/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/junyihong/boardproject/controller/ArticleControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
