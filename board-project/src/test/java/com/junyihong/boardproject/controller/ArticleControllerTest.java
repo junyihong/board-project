7bfdfab8b81601812eeea070c290320c5fe8f60b
junyihong <junyihong@naver.com>
2023-07-31 16:39:53 +0900

    #18 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러를 테스트.
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록
    `@Import` 추가

--- board-project/src/test/java/com/junyihong/boardproject/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/junyihong/boardproject/controller/ArticleControllerTest.java
@@ -1,16 +1,19 @@
 package com.junyihong.boardproject.controller;
 
+import com.junyihong.boardproject.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Import(SecurityConfig.class)
 @DisplayName("View 컨트롤러 - 게시글")
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
