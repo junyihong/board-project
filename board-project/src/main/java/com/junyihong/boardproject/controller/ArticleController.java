4ab91f01336ea41128f84d8c15fa7483bfbd0488
junyihong <junyihong@naver.com>
2023-08-03 08:55:57 +0900

    #28 - 페이지네이션 기능을 컨트롤러 (게시판 뷰)에 반영
    테스트는 'Page' 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현에서 이어서 활용할 전망

--- board-project/src/main/java/com/junyihong/boardproject/controller/ArticleController.java
+++ board-project/src/main/java/com/junyihong/boardproject/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.junyihong.boardproject.domain.type.SearchType;
 import com.junyihong.boardproject.dto.response.ArticleResponse;
 import com.junyihong.boardproject.dto.response.ArticleWithCommentsResponse;
 import com.junyihong.boardproject.service.ArticleService;
+import com.junyihong.boardproject.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,12 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -30,7 +35,11 @@ public class ArticleController {
             ModelMap map
             )
     {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
         return "articles/index";
     }
 
