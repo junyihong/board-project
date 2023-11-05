4bdd1b0aa4f3060124432aa52a9220556c9a585a
junyihong <junyihong@naver.com>
2023-11-05 13:38:53 +0900

    #66 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/main/java/com/junyihong/boardproject/config/SecurityConfig.java
+++ src/main/java/com/junyihong/boardproject/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.junyihong.boardproject.config;
 
 import com.junyihong.boardproject.dto.UserAccountDto;
 import com.junyihong.boardproject.dto.security.BoardPrincipal;
+import com.junyihong.boardproject.dto.security.KakaoOAuth2Response;
 import com.junyihong.boardproject.repository.UserAccountRepository;
+import com.junyihong.boardproject.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +14,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+                                                   ) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
@@ -41,23 +51,61 @@ public class SecurityConfig {
                 )
                 .formLogin(login -> login
                         .loginPage("/login") // 커스텀 로그인 페이지 경로
-                        .defaultSuccessUrl("/")
+                        .defaultSuccessUrl("/", true)
                 )
-                .logout()
-                .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-                .and()
+                .oauth2Login(oAuth -> oAuth
+                        .loginPage("/oauth2/authorization/kakao") // OAuth2 로그인도 커스텀 로그인 페이지로 설정
+                        .defaultSuccessUrl("/", true)
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)
+                        )
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/login"))
                 .csrf().disable() //csrf 비활성화
                 .build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
-        return username -> userAccountRepository
-                .findById(username)
-                .map(UserAccountDto::from)
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+        return username -> userAccountService
+                .searchUser(username)
+//                .map(UserAccountDto::from)
                 .map(BoardPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            UserAccountService userAccountService,
+            PasswordEncoder passwordEncoder
+    ) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        // 한번만 사용하기 때문에 구현체를 new 키워드로 생성
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            KakaoOAuth2Response kakaoOAuth2Response = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId(); // "kakao"
+            String providerId = String.valueOf(kakaoOAuth2Response.id());
+            String username = registrationId + "_" + providerId;
+            String dummyPassword = passwordEncoder.encode("{bcrypt}" + UUID.randomUUID());
+            // 패스워드의 인증 책임은 kakao에 있으나, 유저 어카운트에 패스워드 컬럼이 notNull 이므로 더미 패스워드를 설정해준다.
+
+            return userAccountService.searchUser(username)
+                    .map(BoardPrincipal::from)
+                    .orElseGet(() ->
+                            BoardPrincipal.from(
+                                    userAccountService.saveUser(
+                                            username,
+                                            dummyPassword,
+                                            kakaoOAuth2Response.email(),
+                                            kakaoOAuth2Response.nickname(),
+                                            null
+                                    )
+                            )
+                    );
+        };
     }
 
     @Bean
